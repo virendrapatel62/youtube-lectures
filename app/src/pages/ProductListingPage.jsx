2d3a38a444e6a3ba9ad2721feb996646f833df78
Virendra patel <patelvirendra62@gmail.com>
2023-08-06 11:22:33 +0530

    Added Api call

--- app/src/pages/ProductListingPage.jsx
+++ app/src/pages/ProductListingPage.jsx
@@ -1,5 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+function ProductList(props) {
+  return (
+    <ul>
+      {props.products.map((p) => (
+        <li key={p.id}>
+          <Link to={`/store/dp/${p.id}`}>{p.title}</Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
 
 export default function ProductListingPage() {
-  return <div>ProductListingPage</div>;
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://fakestoreapi.com/products").then((response) => {
+      setProducts(response.data);
+    });
+  }, []);
+
+  return (
+    <div>
+      <h1>Products</h1>
+      <hr />
+
+      <ProductList products={products} />
+    </div>
+  );
 }
