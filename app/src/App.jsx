42aaa77030a9d08dedd0cff81b717fda2989437e
Virendra patel <patelvirendra62@gmail.com>
2023-08-06 11:02:52 +0530

    created react projecr

---
+++ app/src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
+        veniam exercitationem consequatur maiores vitae odit, nostrum debitis
+        enim ipsa in minus dicta? Magni dolorum quos dicta ut nisi minima
+        nesciunt?
+      </p>
+    </div>
+  );
+}
+
+export default App;
